6ce8236fb035d35380eba5a4544b2e7076a0609c
SunshineNURSmile <yuanyiw@bu.edu>
2023-05-03 19:59:45 -0400

    Disabled preflight

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{jsx, js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{jsx, js}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	corePlugins: {
+		preflight: false,
+	},
+};

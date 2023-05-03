a16854a0c9f4875fc23555c32a2a034159310782
SunshineNURSmile <yuanyiw@bu.edu>
2023-05-03 19:13:07 -0400

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-const App = () => {
-  return (
-    <div>Culi</div>
-  )
-}
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
 
-export default App
+const App = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<Home />}></Route>
+				<Route path='/login' element={<Login />}></Route>
+				<Route path='/signup' element={<Signup />}></Route>
+				<Route path='/profile' element={<Profile />}></Route>
+			</Route>
+		</Routes>
+	);
+};
+
+export default App;

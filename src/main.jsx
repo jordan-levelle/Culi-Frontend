949b3e11d0ef0157794ca6718270c33022d05c2e
SunshineNURSmile <yuanyiw@bu.edu>
2023-05-03 19:13:33 -0400

    Added router, rtk, and mantine provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import { MantineProvider } from "@mantine/core";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<MantineProvider withGlobalStyles withNormalizeCSS>
+					<App />
+				</MantineProvider>
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+);

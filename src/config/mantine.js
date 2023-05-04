d20c4205c0cb53aaf2eaccba7ccab966f3b0e23e
SunshineNURSmile <yuanyiw@bu.edu>
2023-05-04 03:01:34 -0400

    Added Global Theme Object

--- src/config/mantine.js
+++ src/config/mantine.js
@@ -1,3 +1,9 @@
 const themeObject = {
-  
-}
+	colorScheme: "light",
+	primaryColor: "green",
+	defaultRadius: "lg",
+	defaultGradient: { from: "teal", to: "lime", deg: 105 },
+	loader: "bars",
+};
+
+export default themeObject;
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,15 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import { MantineProvider } from "@mantine/core";
+import themeObject from "./config/mantine";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
-				<MantineProvider withGlobalStyles withNormalizeCSS>
+				<MantineProvider theme={themeObject} withGlobalStyles withNormalizeCSS>
 					<App />
 				</MantineProvider>
 			</BrowserRouter>

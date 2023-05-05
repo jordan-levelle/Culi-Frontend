e4eb9e077797d7322d2e9d1bb8d08a33cccbad04
SunshineNURSmile <yuanyiw@bu.edu>
2023-05-04 22:59:22 -0400

    Added error handling

--- src/components/authentication/EmailSignup.jsx
+++ src/components/authentication/EmailSignup.jsx
@@ -1,16 +1,35 @@
+import { useNavigate } from "react-router-dom";
+
 import {
 	createUserWithEmailAndPassword,
 	sendEmailVerification,
 	updateProfile,
+	deleteUser,
 } from "firebase/auth";
 import { auth } from "../../config/firebase";
+import { useLogoutMutation } from "../../app/api/authApiSlice";
 
-import { TextInput, Button, Group, Box, Divider,Center } from "@mantine/core";
+import {
+	TextInput,
+	PasswordInput,
+	Button,
+	Group,
+	Box,
+	Divider,
+	Center,
+	Title,
+	Flex,
+} from "@mantine/core";
 import { useForm, isEmail, hasLength, matchesField } from "@mantine/form";
+import { notifications } from "@mantine/notifications";
 
 import GoogleLogin from "./GoogleLogin";
+import Logo from "../Logo";
+import Spinner from "../Spinner";
 
-const EmailSignup = () => {
+const EmailSignup = ({ setActive }) => {
+	const navigate = useNavigate();
+
 	const form = useForm({
 		initialValues: {
 			username: "",
@@ -32,6 +51,8 @@ const EmailSignup = () => {
 		},
 	});
 
+	const [logout, { isLoading }] = useLogoutMutation();
+
 	const signupWithEmail = async (credentials) => {
 		try {
 			await createUserWithEmailAndPassword(
@@ -43,20 +64,52 @@ const EmailSignup = () => {
 				displayName: credentials.username,
 			});
 			await sendEmailVerification(auth.currentUser);
+			setActive(1);
 		} catch (error) {
-			console.error(error);
+			let message = "";
+			switch (error.code) {
+				case "auth/email-already-in-use":
+					message = "This email is already registered, please login";
+					navigate("/login");
+					break;
+				case "auth/invalid-email":
+					message = "Please enter a valid Email address";
+					break;
+				case "auth/weak-password":
+					message = "Your password is weak, please use a strong password";
+				default:
+					message = "There is an error, please try again";
+					await deleteUser(auth.currentUser);
+					break;
+			}
+			notifications.show({
+				title: "Error Signing Up",
+				message,
+				color: "red",
+				withCloseButton: true,
+				autoClose: 5000,
+			});
+			await logout();
 		}
 	};
 
+	if (isLoading) {
+		return <Spinner />;
+	}
+
 	return (
 		<Box
-      className="rounded-lg shadow-lg p-8"
+			className='rounded-lg shadow-lg p-8'
 			component='form'
 			maw={400}
 			mx='auto'
+			mt={20}
 			onSubmit={form.onSubmit((values) => signupWithEmail(values))}
 		>
-      
+			<Flex direction='column' align='center' gap={6}>
+				<Logo />
+				<Title order={5}>Cultivate the Sense</Title>
+			</Flex>
 			<TextInput
 				label='Username'
 				withAsterisk
@@ -70,16 +123,14 @@ const EmailSignup = () => {
 				mt='md'
 				{...form.getInputProps("email")}
 			/>
-			<TextInput
+			<PasswordInput
 				label='Password'
-				type='password'
 				withAsterisk
 				mt='md'
 				{...form.getInputProps("password")}
 			/>
-			<TextInput
+			<PasswordInput
 				label='Confirm Password'
-				type='password'
 				withAsterisk
 				mt='md'
 				{...form.getInputProps("confirmPassword")}
